00bddd611dc4d5d731c337e590d598fd97f11e20
Martin <micros70@outlook.com>
2016-11-24 22:57:19 -0500

    First Commit

---
+++ OutlookPluginReaderWeb/Functions/FunctionFile.js
@@ -0,0 +1,16 @@
+﻿Office.initialize = function () {
+}
+
+// Fonction d'assistance pour ajouter un message d'état à la barre d'informations.
+function statusUpdate(icon, text) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+    type: "informationalMessage",
+    icon: icon,
+    message: text,
+    persistent: false
+  });
+}
+
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!");
+}
---
+++ OutlookPluginReaderWeb/MessageRead.js
@@ -0,0 +1,98 @@
+﻿/// <reference path="/Scripts/FabricUI/MessageBanner.js" />
+
+(function () {
+  "use strict";
+
+  var messageBanner;
+
+  // La fonction d'initialisation Office doit être exécutée chaque fois qu'une nouvelle page est chargée.
+  Office.initialize = function (reason) {
+    $(document).ready(function () {
+      var element = document.querySelector('.ms-MessageBanner');
+      messageBanner = new fabric.MessageBanner(element);
+      messageBanner.hideBanner();
+      loadProps();
+    });
+  };
+
+  // Prend un tableau d'objets AttachmentDetails, puis crée une liste de noms de pièces jointes séparés par un saut de ligne.
+  function buildAttachmentsString(attachments) {
+    if (attachments && attachments.length > 0) {
+      var returnString = "";
+      
+      for (var i = 0; i < attachments.length; i++) {
+        if (i > 0) {
+          returnString = returnString + "<br/>";
+        }
+        returnString = returnString + attachments[i].name;
+      }
+
+      return returnString;
+    }
+
+    return "None";
+  }
+
+  // Met en forme un objet EmailAddressDetails en tant que
+  // GivenName Surname <emailaddress>
+  function buildEmailAddressString(address) {
+    return address.displayName + " &lt;" + address.emailAddress + "&gt;";
+  }
+
+  // Prend un tableau d'objets EmailAddressDetails, puis
+  // crée une liste de chaînes mises en forme, séparées par un saut de ligne
+  function buildEmailAddressesString(addresses) {
+    if (addresses && addresses.length > 0) {
+      var returnString = "";
+
+      for (var i = 0; i < addresses.length; i++) {
+        if (i > 0) {
+          returnString = returnString + "<br/>";
+        }
+        returnString = returnString + buildEmailAddressString(addresses[i]);
+      }
+
+      return returnString;
+    }
+
+    return "None";
+  }
+
+  // Charge les propriétés à partir de l'objet de base Item, puis charge les
+  // propriétés spécifiques au message.
+  function loadProps() {
+    var item = Office.context.mailbox.item;
+
+    $('#dateTimeCreated').text(item.dateTimeCreated.toLocaleString());
+    $('#dateTimeModified').text(item.dateTimeModified.toLocaleString());
+    $('#itemClass').text(item.itemClass);
+    $('#itemId').text(item.itemId);
+    $('#itemType').text(item.itemType);
+
+    $('#message-props').show();
+
+    $('#attachments').html(buildAttachmentsString(item.attachments));
+    $('#cc').html(buildEmailAddressesString(item.cc));
+    $('#conversationId').text(item.conversationId);
+    $('#from').html(buildEmailAddressString(item.from));
+    $('#internetMessageId').text(item.internetMessageId);
+    $('#normalizedSubject').text(item.normalizedSubject);
+    $('#sender').html(buildEmailAddressString(item.sender));
+    $('#subject').text(item.subject);
+    $('#to').html(buildEmailAddressesString(item.to));
+    item.body.getAsync(
+        "text",
+        { asyncContext: "Context callback" },
+        function callback(result) {
+            $('#bodySize').text(result.value.trim().split(" ").length);
+        });
+  }
+
+  // Fonction d'assistance pour afficher les notifications
+  function showNotification(header, content) {
+    $("#notificationHeader").text(header);
+    $("#notificationBody").text(content);
+    messageBanner.showBanner();
+    messageBanner.toggleExpansion();
+  }
+})();
